4472bb9c34bf1515591c1981e9b7a440cac47911
chencchen <39607456+chencchen@users.noreply.github.com>
2020-04-30 01:39:42 +0800

    Create source.js

---
+++ 使用ast对某v5加密进行还原/source.js
@@ -0,0 +1,51 @@
+/*
+ * 加密工具已经升级了一个版本，目前为 sojson.v5 ，主要加强了算法，以及防破解【绝对不可逆】配置，耶稣也无法100%还原，我说的。;
+ * 已经打算把这个工具基础功能一直免费下去。还希望支持我。
+ * 另外 sojson.v5 已经强制加入校验，注释可以去掉，但是 sojson.v5 不能去掉（如果你开通了VIP，可以手动去掉），其他都没有任何绑定。
+ * 誓死不会加入任何后门，sojson JS 加密的使命就是为了保护你们的Javascript 。
+ * 警告：如果您恶意去掉 sojson.v5 那么我们将不会保护您的JavaScript代码。请遵守规则
+ * 新版本: https://www.jsjiami.com/ 支持批量加密，支持大文件加密，拥有更多加密。 */
+
+// 第一部分已经移到module文件中去了
+
+//这是第二部分
+var a = {}
+    , b = {};
+(function(_0x117d09, _0x110647) {
+    var _0x4845bf = {
+        'XhuzS': _0x4d94('0x0', 'W7bQ'),
+        'iUTPo': _0x4d94('0x1', ']2T3')
+    };
+    _0x117d09[_0x4d94('0x2', 'WIC5')] = _0x4845bf['XhuzS'];
+    _0x110647['adinfo'] = _0x4845bf[_0x4d94('0x3', 'Wka7')];
+    _0x110647[_0x4d94('0x4', 'D8d!')] = '如果您的JS里嵌套了PHP，JSP标签，等等其他非JavaScript的代码，请提取出来再加密。这个工具不能加密php、jsp等模版内容';
+}(a, b));
+;(function(_0x182aaa, _0x4c3590, _0x55392e) {
+    var _0x4b40d0 = {
+        'gTfic': _0x4d94('0x5', 'oqkX'),
+        'GXIQH': function _0x38020c(_0x65ebe0, _0x2dc82b) {
+            return _0x65ebe0 === _0x2dc82b;
+        },
+        'xyVxG': _0x4d94('0x6', 'JsgM'),
+        'qeHKL': function _0x3e069c(_0x501f24, _0x41e6d1) {
+            return _0x501f24 + _0x41e6d1;
+        },
+        'EkYag': _0x4d94('0x7', '^7hL'),
+        'YZgLH': _0x4d94('0x8', 'g@wB')
+    };
+    _0x55392e = 'al';
+    try {
+        _0x55392e += _0x4b40d0['gTfic'];
+        _0x4c3590 = encode_version;
+        if (!(typeof _0x4c3590 !== _0x4d94('0x9', 'oqkX') && _0x4b40d0[_0x4d94('0xa', 'M*Wm')](_0x4c3590, _0x4b40d0['xyVxG']))) {
+            _0x182aaa[_0x55392e](_0x4b40d0[_0x4d94('0xb', '7glO')]('删除', _0x4b40d0['EkYag']));
+        }
+    } catch (_0x781e53) {
+        if ('aYN' === _0x4d94('0xc', '^7hL')) {
+            _0x182aaa[_0x55392e](_0x4d94('0xd', '9z79'));
+        } else {
+            _0x182aaa[_0x55392e](_0x4b40d0[_0x4d94('0xe', 'oqkX')]);
+        }
+    }
+}(window));
+;encode_version = 'sojson.v5';
